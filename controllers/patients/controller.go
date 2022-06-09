a052042e4cdf5264503e571512797f4bd1464732
YD <ydglitch@gmail.com>
2022-06-09 13:30:50 +0000

    add patient handler

--- controllers/patients/controller.go
+++ controllers/patients/controller.go
@@ -1,9 +1,19 @@
-package books
+package patients
 
 import (
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 type handler struct {
 	DB *gorm.DB
+}
+
+func RegRoutes(r *gin.Engine, db *gorm.DB) {
+	h := &handler{
+		DB: db,
+	}
+
+	route := r.Group("/patients")
+	route.POST("/", h.NewPatientHandler)
 }
